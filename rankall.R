7f53e6214b7a5effd42992891dfba74adf870093
KarenEP <karen.s.trullas@gmail.com>
2015-09-10 01:32:34 +0200

    Loop for all states and ordered rank

--- rankall.R
+++ rankall.R
@@ -8,16 +8,39 @@ rankall <- function(outcome, num = "best") {
   ## Check that state and outcome are valid
   outcomes<-c("heart attack","heart failure","pneumonia")
   for(i in 1:3){
-    condition<-outcome==outcomes[i]
-    if(condition==TRUE){break}
+    condition<-outcome==outcomes[i]                 ##Logical vector to check outcome given
+    if(condition==TRUE){break}                      ##Correct outcome given break the loop
   }
-    if(condition==FALSE){stop("Invalid outcome")}
+    if(condition==FALSE){stop("Invalid outcome")}   ##Incorrect outcome given stop the function
   
+  states<-unique(data[ ,"State"])                   ##Character vector of all states(no repeted)
+  nstates<-length(states)                           ##Number of states [54]
   
+  out<-c("heart attack"=11,"heart failure"=17,"pneumonia"=23)
+  x<-out[outcome]                 
+  rank<-data.frame()
   
-  
-
   ## For each state, find the hospital of the given rank
+  for(i in 1:nstates){                              ##Loop for all states
+   dataout<-data[data[ ,"State"]==states[i], ]    ##Only data of the state loop given
+   list<-dataout[ ,c(2,7,x)]                      ##Data frame of hospitalname,state,outcome
+   list<-list[complete.cases(list), ]             ##Na cleaning
+   
+   hname<-list[ ,1]
+   outcome<-list[ ,3]
+   orderlist<-list[order(outcome,hname,decreasing=FALSE)]
+   if(num=="best"){
+     select<-orderlist[1, ]
+     rank<-rbind(rank,select)
+   }
+   if(num!="best"){
+     select<-orderlist[num, ]
+     rank<-rbind(rank,select)
+   }
+  }
+  
+  ##minimum<-min(list[ ,3])
+  
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
 
